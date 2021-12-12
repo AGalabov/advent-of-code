f380a4cdf78419148866cf1ff0f8def8c835cf1c
Alexandar Galabov <alexandar_1997@abv.bg>
2021-12-12 19:14:27 +0200

    add solutions to puzzles from day12

---
+++ day12/puzzle1.ts
@@ -0,0 +1,61 @@
+import { readInput } from '../utils';
+
+class CaveMap {
+  private caveRoutes: Map<string, string[]>;
+
+  constructor() {
+    this.caveRoutes = new Map();
+  }
+
+  addLink(cave1: string, cave2: string) {
+    const routesCave1 = this.caveRoutes.get(cave1) ?? [];
+    this.caveRoutes.set(cave1, [...routesCave1, cave2]);
+
+    const routesCave2 = this.caveRoutes.get(cave2) ?? [];
+    this.caveRoutes.set(cave2, [...routesCave2, cave1]);
+  }
+
+  findPaths() {
+    return this.findPathsRecursive('start', []);
+  }
+
+  private findPathsRecursive(from: string, usedSmallCaves: string[]): number {
+    let visitedSmallCaves = [...usedSmallCaves];
+
+    if (/^[a-z]+$/.test(from)) {
+      if (usedSmallCaves.includes(from)) {
+        return 0;
+      }
+      visitedSmallCaves.push(from);
+    }
+
+    if (from === 'end') {
+      return 1;
+    }
+
+    const caves = this.caveRoutes.get(from);
+
+    if (!caves) {
+      throw new Error('Initial cave name not linked to other caves');
+    }
+
+    return caves.reduce((acc, curr) => {
+      return acc + this.findPathsRecursive(curr, visitedSmallCaves);
+    }, 0);
+  }
+}
+
+function solve(fileName: string) {
+  const input = readInput(fileName);
+
+  const caveMap = new CaveMap();
+
+  input.forEach((line) => {
+    const [cave1, cave2] = line.split('-');
+    caveMap.addLink(cave1, cave2);
+  });
+
+  return caveMap.findPaths();
+}
+
+console.log('Solution:', solve('./input'));
