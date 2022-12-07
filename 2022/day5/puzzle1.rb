df9ca46fb959141e30e5778f4c956e546db2e4e1
Georgy Angelov <georgyangelov@users.noreply.github.com>
2022-12-07 13:10:01 +0200

    Update puzzle1.rb

--- 2022/day5/puzzle1.rb
+++ 2022/day5/puzzle1.rb
@@ -1,5 +1,7 @@
 
 class SupplyStacks
+  # Бих го направил да се подава стринг, така ако искаш можеш да му пишеш unit тестове по-лесно,
+  # а разликата в извикването не е голяма
   def initialize(file_name)
     @file_name = file_name
   end
@@ -22,16 +24,30 @@ class SupplyStacks
     setup_input = setup.split("\n")
 
     stacks_input = setup_input.last
-    setup_lines = setup_input[0..-2].reverse
+    
+    # Може да стане с drop
+    # setup_lines = setup_input[0..-2].reverse
+    setup_lines = setup_input.reverse.drop(2)
 
     stacks = stacks_input.split(" ")
 
-    @stacks = stacks.map.with_index{ |_, index| setup_lines.map{ |line| line[4*index + 1] }.filter{ |val| val != " " } }
+    # Прекалено много нестване на inline блокове, напиши го с multiline блок
+    # @stacks = stacks.map.with_index{ |_, index| setup_lines.map{ |line| line[4*index + 1] }.filter{ |val| val != " " } }
+    
+    # https://ruby-doc.org/core-2.4.1/Array.html#method-i-each_index
+    @stacks = stacks.each_index do |index|
+      setup_lines
+        .map { |line| line[4*index + 1] }
+        .filter { |val| val != " " } }
+    end
   end
 
+  # Тези функции по-скоро трябва да са private
   def follow_instructions(instructions)
+    # Това май не се използва никъде?
     format = /([\d]*)/
 
+    # instr -> instruction :)
     parsed = instructions.split("\n").map { |instr| instr.scan(/\d+/).map(&:to_i) }
 
     parsed.each do |line|
